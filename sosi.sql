872bfbfdcc8da6e614545b408db80988ea61cb40
sungmin <68140696+lsm15111@users.noreply.github.com>
2022-08-09 16:59:17 +0900

    sql

---
+++ sosi.sql
@@ -0,0 +1,13 @@
+drop table sosi;
+create table sosi (
+id number(4) primary key ,
+name varchar2(10) ,
+salary number ,
+birth date ,
+email varchar2(20) ,
+grade varchar2(4)
+);
+commit;
+desc sosi;
+select *from sosi;
+
