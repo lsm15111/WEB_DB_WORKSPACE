f4f03503a2e4472980de11e77a65aea7e418b88d
sungmin <lsm1511@naver.com>
2022-08-18 17:51:19 +0900

    eclipse

---
+++ work/Web0812/src/main/java/net/hb/common/Two.java
@@ -0,0 +1,14 @@
+package net.hb.common;
+
+public class Two {
+  private int doorkey=1234 ;
+  
+  public Two() { }
+  
+  public void book() {
+    String title="몽블랑";
+    System.out.println(doorkey);
+    System.out.println(title);
+  }//end
+  
+}//class END
