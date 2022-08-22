f107e4c653c1f1bb32349f0dc816de9ee05220c7
sungmin <lsm1511@naver.com>
2022-08-22 15:38:04 +0900

    testbean

---
+++ work/Web0812/src/main/java/net/hb/common/TestBean.java
@@ -0,0 +1,18 @@
+package net.hb.common;
+
+public class TestBean { //Bean=DTO=VO 필드/getter/setter==한강다리역할=비행기역할=ktx역할
+  private String title;
+  private int pay;
+  private java.util.Date  wdate;
+  private int code;
+  
+  public String getTitle() {return title;}
+  public void setTitle(String title) {this.title = title;	}
+  public int getPay() {return pay;}
+  public void setPay(int pay) {this.pay = pay;	}
+  public java.util.Date getWdate() {return wdate;	}
+  public void setWdate(java.util.Date wdate) {this.wdate = wdate;	}
+  public int getCode() {return code;	}
+  public void setCode(int code) {	this.code = code;}
+  
+}//class END
