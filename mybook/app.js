212d32404d77f0fcd6c3f2bf087163a49baaeaf5
sungmin <lsm1511@naver.com>
2022-08-23 16:10:05 +0900

    app

--- mybook/app.js
+++ mybook/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testRouter = require('./routes/test');
-
+var snowRouter = require('./routes/snow');
 var app = express();
 
 // view engine setup
@@ -22,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/cake',testRouter);
+app.use('/test',testRouter);
+app.use('/snow',snowRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
