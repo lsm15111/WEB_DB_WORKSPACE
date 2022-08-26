ea2b9ff1236e54103ed5688e016651bb5536e571
sungmin <lsm1511@naver.com>
2022-08-26 17:18:23 +0900

    index main

--- mybook/routes/index.js
+++ mybook/routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express 08-23-화요일' });
-  res.send('<h1><font color=blue>hello Go GilDong 고길동</font></h1>');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
