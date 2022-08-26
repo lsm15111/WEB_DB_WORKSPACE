ceeaf2ed71f7d04dfbf3cada6657e9703ff2aa4a
sungmin <lsm1511@naver.com>
2022-08-26 17:16:12 +0900

    coronachart

--- mybook/routes/coronachart.js
+++ mybook/routes/coronachart.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  //res.send('winter1 wind1 snow1');
-  res.render('snow', { msg: 'winter2 wind2 snow2'});
+  res.render('coronachart', { title: 'coronachart' });
 });
 
 module.exports = router;
--- mybook/routes/coronapicker.js
+++ mybook/routes/coronapicker.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* 08-24-수요일 */  
 router.get('/', function(req, res, next) {
-  //res.send('test test');
-  res.render('test', { title: 'title mytest 2:03' });
+  res.render('coronapicker', { title: 'coronapicker' });
 });
 
 module.exports = router;
